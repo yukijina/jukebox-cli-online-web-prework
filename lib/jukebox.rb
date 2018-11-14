b44075ed2fd3233ef76360fc7b222d6178164749
Yuki N <lunarbase@outlook.com>
2018-11-14 19:34:25 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -38,8 +38,8 @@ def play(songs)
     num = index +1
     if user_input == num || user_input == song
       output = "Playing #{song}"
-    else 
-      output = "Invalid input, please try again"
+    #else 
+      #output = "Invalid input, please try again"
     end
   end
   puts "#{output}"
