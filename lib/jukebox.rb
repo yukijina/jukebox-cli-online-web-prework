c10e62b12b2547e04f395b5283182ccf864297c9
Yuki N <lunarbase@outlook.com>
2018-11-14 19:05:52 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,8 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.chomp 
   list(songs)
-  if user_input ==
+  if user_input == index+1 || user_input == song 
+    puts "Playing #{song}"
 end
   
 
